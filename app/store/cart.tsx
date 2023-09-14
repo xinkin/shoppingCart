417adf38e02cfe55fb38283d9deaa9d3e1cd65b8
Yash Chowdhury <yashchowdhurysn@gmail.com>
2023-09-14 21:11:31 +0530

    fixed local storage

--- app/store/cart.tsx
+++ app/store/cart.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { create } from 'zustand'
 import { nanoid } from 'nanoid'
 
@@ -8,27 +7,40 @@ interface Product {
   description: string
   price: number
 }
+
 interface CartState {
   cart: Product[]
   addToCart: (product: Product) => void
   removeFromCart: (productId: string) => void
 }
-const useStore = create<CartState>()((set) => ({
-  cart: JSON.parse(localStorage.getItem('cart')) || [],
-  addToCart: (product) =>
-    set((state) => {
-      const updatedCart = [...state.cart, { ...product, id: nanoid() }]
-      localStorage.setItem('cart', JSON.stringify(updatedCart))
-      return { cart: updatedCart }
-    }),
-  removeFromCart: (productId) =>
-    set((state) => {
-      const updatedCart = state.cart.filter(
-        (product) => product.id !== productId,
-      )
-      localStorage.setItem('cart', JSON.stringify(updatedCart))
-      return { cart: updatedCart }
-    }),
-}))
+
+const useStore = create<CartState>((set) => {
+  const initialCart =
+    typeof window !== 'undefined'
+      ? JSON.parse(localStorage.getItem('cart')) || []
+      : []
+
+  return {
+    cart: initialCart,
+    addToCart: (product) =>
+      set((state) => {
+        const updatedCart = [...state.cart, { ...product, id: nanoid() }]
+        if (typeof window !== 'undefined') {
+          localStorage.setItem('cart', JSON.stringify(updatedCart))
+        }
+        return { cart: updatedCart }
+      }),
+    removeFromCart: (productId) =>
+      set((state) => {
+        const updatedCart = state.cart.filter(
+          (product) => product.id !== productId,
+        )
+        if (typeof window !== 'undefined') {
+          localStorage.setItem('cart', JSON.stringify(updatedCart))
+        }
+        return { cart: updatedCart }
+      }),
+  }
+})
 
 export default useStore
